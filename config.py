105dfd6a8164ed6b2a4eeaa6acb329ab232c0573
Diego V <diego.v@bineo.com>
2021-10-21 22:41:18 -0500

    Test para CPs sin asentamientos. Agregar warnings.

---
+++ config.py
@@ -0,0 +1,7 @@
+
+DEFAULT_ENV = "staging"
+
+ENV_URLS = {
+    "local"     : "http://localhost:5000/v1/catalogs/get-zipcode-neighborhoods", 
+    "staging"   : "https://wap-prod-catalogs-dev.azurewebsites.net/v1/catalogs/get-zipcode-neighborhoods"
+}
