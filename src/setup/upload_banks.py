a4da53cc5987a586a72fb66b1c0f1db358aa0074
Diego V <diego.v@bineo.com>
2022-01-14 18:17:40 -0600

    Unificar claves de bancos
    Signed-off-by: Diego Villamil <diego.v@bineo.com>

--- src/setup/upload_banks.py
+++ src/setup/upload_banks.py
@@ -20,9 +20,9 @@ local_path   = SITE/"refs/catalogs"
 storage_path = "product/epic-catalogs/app-services"
 
 ctlg_files = {
-    "banks"  : "national-banks.feather",
-    "plazas" : "national-banks-plazas.feather",
-    "bins"   : "national-banks-bins.feather"
+    "banks"  : "national-banks",
+    "plazas" : "national-banks-plazas",
+    "bins"   : "national-banks-bins"
 }
 
 #%% Banks
@@ -38,12 +38,11 @@ df_colnames = {
 ctlg_df = (read_excel_table(*excel_call)
     .rename(columns=df_colnames)
     .assign(warning=lambda df: (df.is_active != 'A'))
-    .loc[:, df_colnames.values()]
-    .drop(columns=["is_active", "type"])
-    .astype(str)    
-    .reset_index(drop=True))
+    .loc[:, ["name", "code"]]
+    .astype(str)
+    .reset_index())
 
-ctlg_df.to_feather(local_path/ctlg_files["banks"])
+ctlg_df.to_feather(local_path/f"{ctlg_files['banks']}.feather")
 
 #%% Plazas is almost the same. 
 
@@ -53,53 +52,54 @@ df_colnames = {
     "Nombre" : "name",
     "Plaza"  : "code"}
 
-# Setup columns
 ctlg_df = (read_excel_table(*excel_call)
     .rename(columns=df_colnames)
     .assign(code=lambda df: df.code.map(str).str.pad(3, fillchar="0")))
 
-ctlg_df.to_feather(local_path/ctlg_files["plazas"])
+ctlg_df.to_feather(local_path/f"{ctlg_files['plazas']}.feather")
 
 
 #%% Bins requires some column manipulation
 
 the_types   = {"integer": int, "character": str, "date": "datetime64[ns]"}
 
-excel_call = [local_path/"api-catalogs.xlsx.lnk", "banks-bins", "cols_anexo1"]
-bins_cols_ = read_excel_table(*(excel_call))
-bins_cols  = (bins_cols_.loc[~bins_cols_.Tipo.isnull(), ]
-    .set_index("Columna")["Tipo"])
+excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "cols_anexo1")
+bins_cols_ = read_excel_table(*excel_call)
+bins_cols  = { row["Ref Nombre"]: row["Tipo"] 
+        for _, row in bins_cols_.iterrows() if row["Tipo"] }
 
-excel_call = [local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1c"]
-bins_1c = (read_excel_table(*(excel_call))
-    .loc[:, bins_cols.keys()]
+excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1c")
+bins_1c = (read_excel_table(*excel_call)[bins_cols.keys()]
     .astype({k: the_types[v] for (k, v) in bins_cols.items()}))
 
-excel_call = [local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1"]
+excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1")
 bins_1 = (read_excel_table(*(excel_call))
     .loc[:, bins_cols.keys()]
     .astype({k: the_types[v] for (k, v) in bins_cols.items()})
     .set_index("BIN"))
 bins_1.update(bins_1c)
 
-bins = bins_1.reset_index()
+bins = (bins_1
+    .assign(ID = lambda df: df["Id Institución"].astype(str).str[3:])
+    .reset_index())
 
-bins.to_feather(local_path/ctlg_files["bins"])
+bins.to_feather(local_path/f"{ctlg_files['bins']}.feather")
 
 
 #%% And now upload all. 
 
+# Development Mode keeps Env variables. 
 credential = ClientSecretCredential(**{k: os.getenv(v) 
         for (k, v) in ENV_VARS["app_sp"].items()})
 
 blob_container = BlobServiceClient("https://lakehylia.blob.core.windows.net/", credential)
 
 for each_file in ctlg_files.values(): 
-    local_p  = local_path/each_file
-    stg_path = f"{storage_path}/{each_file}"
+    local_p  = local_path/f"{each_file}.feather"
+    stg_path = f"{storage_path}/{each_file}.feather"
 
     the_blob = blob_container.get_blob_client(container="bronze", blob=stg_path)
 
     with open(local_p, "rb") as _f: 
-        the_blob.upload_blob(_f)
+        the_blob.upload_blob(_f, overwrite=True)
 
