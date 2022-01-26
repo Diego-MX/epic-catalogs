ef2e1a659ec657a839ce49590511df0d148b5c56
Diego V <diego.v@bineo.com>
2022-01-25 19:14:59 -0600

    El catálogo de estados tenía mal las claves
    Signed-off-by: Diego Villamil <diego.v@bineo.com>

--- scriptsR/profile.R
+++ scriptsR/profile.R
@@ -8,27 +8,29 @@ filter <- dplyr::filter
 
 readRenviron("../.env")
 
+local({
+    w_repos  <- getOption("repos")
+    w_repos["CRAN"] <- "https://cran.itam.mx"
+    options(repos = w_repos)
+})
 
+.env <- new.env()
+.env$all_equal  <- function (x) all(x %in% x[1])
 
-all_equal <- function (x) all(x %in% x[1])
+.env$not_na     <- function (x) not(is.na(x))
 
-not_exists <- function (file) not(file.exists(file))
+.env$not_in     <- function (x, y_set) not(x %in% y_set)
 
-not_in <- function (x, y_set) not(x %in% y_set)*1
+.env$not_exists <- function (a_file) not(file.exists(a_file))
 
-not_na <- function (x) not(is.na(x))*1
+.env$classes    <- function (a_df) tibble(
+    name  = names(a_df),
+    class = map_chr(a_df, ~(class(.x)[1])))
 
-quotemeta <- function (x) str_replace_all(x, "(\\W)", "\\\\\\1")
+.env$quotemeta  <- function (x) str_replace_all(x, "(\\W)", "\\\\\\1")
 
-str_delatinize <- function (x) chartr("ÁÉÍÓÚÑáéíóúñ", "AEIOUÑaeiouñ", x)
-
-
-classes <- function (a_df) {
-  its_classes_df <- tibble(
-      name = names(a_df), 
-      class = map_chr(a_df, ~(class(.x)[1])) )
-  return (its_classes_df)
-}
+.env$str_delatinize <- function (x) chartr("ÁÉÍÓÚÑáéíóúñ", "AEIOUÑaeiouñ", x)
+attach(.env)
 
 
 list_w_nulls_2_chr <- function (xx) {
@@ -38,7 +40,6 @@ list_w_nulls_2_chr <- function (xx) {
   return (yy)
 }
  
-
 
 k_occurrence <- function (x, dups_only=TRUE) {
   # Integer vector of the occurrence of each value of X. 
@@ -103,4 +104,83 @@ if (interactive()) {
 } else {
   options(readr.num_columns=0)
 }
+
+
+## Configuration
+
+
+
+## Functions
+
+# Para todas las sesiones
+
+
+
+
+# Menos útiles
+
+list_w_nulls_2_chr <- function (xx) {
+  # Double xx and yy to avoid confusion with argument .x,
+  # which is defined implicitely in ~(f(.x)) notation.
+  yy <- map_chr(xx,
+    .f = ~(if (is.null(.x)) "" else as.character(.x) ))
+  return (yy)
+}
+
+k_occurrence <- function (x, dups_only=TRUE) {
+  # Integer vector of the occurrence of each value of X.
+  # When DUPS_ONLY = TRUE, the non-duplicated values become 0.
+  x_tbl <- table(x)
+  if (dups_only) {x_tbl <- x_tbl[x_tbl > 1]}
+  y <- rep(0, length(x))
+  for (x_0 in names(x_tbl)) {y[x == x_0] <- 1:x_tbl[x_0]}
+  return (y)
+}
+
+str_as_key <- function (x, sep="-", dups_only=TRUE) {
+  x_occur <- k_occurrence(x, dups_only)
+  x_ext   <- if_else(x_occur > 0, str_c(sep, x_occur), "")
+
+  y <- str_c(x, x_ext)
+  return (y)
+}
+
+str_codify <- function (x) {
+  y <- x %>% str_delatinize() %>%
+      str_to_lower() %>%
+      str_replace_all(" ", "-")
+  return (y)
+}
+
+tbl_empty <- function (cols, modes) {
+  if (length(modes) == 1) {modes <- rep(modes, length(cols))}
+
+  cols_list <- map(modes, ~vector(.x, 0)) %>% set_names(cols)
+  the_tbl   <- tibble(!!!cols_list)
+  
+  return (the_tbl)
+}
+
+setup_data_dir <- function (the_dir) {
+  if (the_dir %>% not_exists()) {
+    dir.create(the_dir)
+    gitignore_contents(the_dir)
+    
+    dir.create(the_dir %>% file.path("cache"))
+  }
+}
+
+gitignore_contents <- function (the_dir) {
+  ignore_file <- file.path(the_dir, ".gitignore")
+  if (ignore_file %>% not_exists()) {
+    file.create(ignore_file)
+  } else {
+    warning (".gitignore exists in dir: %s" %>% sprintf(the_dir))
+  }
+  lines_for_file <- c("*", "!.gitignore")
+  writeLines(lines_for_file, ignore_file)
+}
+
+
+
 
