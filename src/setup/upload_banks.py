f505f2e52824167ae3811dae6a2683b0e2af0817
Diego V <diego.v@bineo.com>
2022-02-16 20:32:38 -0600

    Agregar columna banxicoID a los bancos.
    Signed-off-by: Diego Villamil <diego.v@bineo.com>

--- src/setup/upload_banks.py
+++ src/setup/upload_banks.py
@@ -3,103 +3,98 @@
 
 # From Excel to Feather to Blob
 
-import os
 
-from azure.identity import ClientSecretCredential 
-from azure.storage.blob import BlobServiceClient
-
+from src.resources import AzureResourcer
 from src.tools import read_excel_table
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-from config import SITE, ENV_VARS
+from config import SITE, ENV
 
 
-#%%  General considerations
-local_path   = SITE/"refs/catalogs"
-storage_path = "product/epic-catalogs/app-services"
+#'  General considerations
+local_path = SITE/'refs/catalogs'
+storage_path = 'product/epic-catalogs/app-services'
 
 ctlg_files = {
-    "banks"  : "national-banks",
-    "plazas" : "national-banks-plazas",
-    "bins"   : "national-banks-bins"
-}
+    'banks'  : 'national-banks',
+    'plazas' : 'national-banks-plazas',
+    'bins'   : 'national-banks-bins'}
 
-#%% Banks
+#' Banks
 
-excel_call  = (local_path/"api-catalogs.xlsx.lnk", "banks", "tabla_297")
+excel_call = (local_path/'api-catalogs.xlsx.lnk', 'banks', 'tabla_297')
 df_colnames = {
-    "NOMBRE" : "name", 
-    "CLAVE"  : "code",  
-    "PARAMETRO Activo / Desactivo": "is_active", 
-    "Tipo banco" : "type", 
-    "warning": "warning"}
+    'NOMBRE' : 'name', 
+    'CLAVE'  : 'code',
+    'BANCO'  : 'banxico_id', 
+    'PARAMETRO Activo / Desactivo': 'is_active', 
+    'Tipo banco' : 'type'}
 
 ctlg_df = (read_excel_table(*excel_call)
     .rename(columns=df_colnames)
-    .assign(warning=lambda df: (df.is_active != 'A'))
-    .loc[:, ["name", "code"]]
+    .loc[:, ['name', 'code', 'banxico_id']]
     .astype(str)
     .reset_index())
 
 ctlg_df.to_feather(local_path/f"{ctlg_files['banks']}.feather")
 
-#%% Plazas is almost the same. 
+#' Plazas is almost the same. 
 
-excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks", "plazas_w")
+excel_call = (local_path/'api-catalogs.xlsx.lnk', 'banks', 'plazas_w')
 
 df_colnames = {
-    "Nombre" : "name",
-    "Plaza"  : "code"}
+    'Nombre' : 'name',
+    'Plaza'  : 'code'}
 
 ctlg_df = (read_excel_table(*excel_call)
     .rename(columns=df_colnames)
-    .assign(code=lambda df: df.code.map(str).str.pad(3, fillchar="0")))
+    .assign(code=lambda df: df.code.map(str).str.pad(3, fillchar='0')))
 
-ctlg_df.to_feather(local_path/f"{ctlg_files['plazas']}.feather")
+ctlg_df.to_feather(local_path/f'{ctlg_files["plazas"]}.feather')
 
 
-#%% Bins requires some column manipulation
+#' Bins requires some column manipulation
 
-the_types   = {"integer": int, "character": str, "date": "datetime64[ns]"}
+the_types = {'integer': int, 'character': str, 'date': 'datetime64[ns]'}
 
-excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "cols_anexo1")
+excel_call = (local_path/'api-catalogs.xlsx.lnk', 'banks-bins', 'cols_anexo1')
 bins_cols_ = read_excel_table(*excel_call)
-bins_cols  = { row["Ref Nombre"]: row["Tipo"] 
-        for _, row in bins_cols_.iterrows() if row["Tipo"] }
+bins_cols = { row['Ref Nombre']: row['Tipo'] 
+        for _, row in bins_cols_.iterrows() if row['Tipo'] }
 
-excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1c")
+excel_call = (local_path/'api-catalogs.xlsx.lnk', 'banks-bins', 'anexo_1c')
 bins_1c = (read_excel_table(*excel_call)[bins_cols.keys()]
     .astype({k: the_types[v] for (k, v) in bins_cols.items()}))
 
-excel_call = (local_path/"api-catalogs.xlsx.lnk", "banks-bins", "anexo_1")
+excel_call = (local_path/'api-catalogs.xlsx.lnk', 'banks-bins', 'anexo_1')
 bins_1 = (read_excel_table(*(excel_call))
     .loc[:, bins_cols.keys()]
     .astype({k: the_types[v] for (k, v) in bins_cols.items()})
-    .set_index("BIN"))
+    .set_index('BIN'))
 bins_1.update(bins_1c)
 
 bins = (bins_1
-    .assign(ID = lambda df: df["Id Institución"].astype(str).str[3:])
+    .assign(ID = lambda df: df['Id Institución'].astype(str).str[3:])
     .reset_index())
 
-bins.to_feather(local_path/f"{ctlg_files['bins']}.feather")
+bins.to_feather(local_path/f'{ctlg_files["bins"]}.feather')
 
 
-#%% And now upload all. 
+#' And now upload all. 
 
 # Development Mode keeps Env variables. 
-credential = ClientSecretCredential(**{k: os.getenv(v) 
-        for (k, v) in ENV_VARS["app_sp"].items()})
 
-blob_container = BlobServiceClient("https://lakehylia.blob.core.windows.net/", credential)
+
+resourcer = AzureResourcer(ENV)
+blob_container = resourcer.get_blob_service()
 
 for each_file in ctlg_files.values(): 
-    local_p  = local_path/f"{each_file}.feather"
-    stg_path = f"{storage_path}/{each_file}.feather"
+    local_p = local_path/f'{each_file}.feather'
+    stg_path = f'{storage_path}/{each_file}.feather'
 
-    the_blob = blob_container.get_blob_client(container="bronze", blob=stg_path)
+    the_blob = blob_container.get_blob_client(container='bronze', blob=stg_path)
 
-    with open(local_p, "rb") as _f: 
+    with open(local_p, 'rb') as _f: 
         the_blob.upload_blob(_f, overwrite=True)
 
