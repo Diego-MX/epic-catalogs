00863ee3119ab27a59b77cf64fb833299e494aa0
Diego V <diego.v@bineo.com>
2022-06-02 20:04:33 -0500

    Hay un error en la validación de las clabes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 SITE = Path(__file__).parent if '__file__' in globals() else Path(os.getcwd())
 
-VERSION = "1.0.33"
+VERSION = "1.0.34"
 
 ENV  = os.environ.get('ENV', 'local')  # local, dev, qas, databricks
 
